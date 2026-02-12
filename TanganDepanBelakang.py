aabb3a0669002d4db7f27a4f5043cbe477e224cc
IndraSeblak <egabhanuindrastasutan@gmail.com>
2026-02-12 15:03:30 +0700

    TanganDepanBelakang.py

--- TanganDepanBelakang.py
+++ TanganDepanBelakang.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2import cv2
 import mediapipe as mp
 
 cap = cv2.VideoCapture(0)
@@ -32,11 +32,13 @@ while True:
                 else:
                     arah = "Front End"
                     warna = (255, 0, 0)
-            cv2.putText(img, f"{label} - {arah}", (20, 60),
+            y_pos = 60 + (idx * 60) 
+            cv2.putText(img, f"{label} - {arah}", (20, y_pos),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.5, warna, 3, cv2.LINE_AA)
             mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     cv2.imshow("webcam", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
+cv2.destroyAllWindows()
 cv2.destroyAllWindows()
